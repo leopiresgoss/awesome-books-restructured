124055f98d81748e6b2d483a97456c90d5ef13f2
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-14 12:12:11 -0600

    Adapt index.html and index.js to update-date work

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { BookShelf, displayBooks } from './modules/render-books/index.js';
 import { setListeners } from './modules/set-listeners/index.js';
+import { updateDate } from './modules/update-date/index.js';
 
 // display all books
 const gBookShelf = new BookShelf();
 displayBooks(gBookShelf);
 setListeners(gBookShelf);
+
+// update user's datetime
+updateDate();
